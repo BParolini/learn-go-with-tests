7bca8018a54f6063b20dc47df18d79ffac381d32
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-09 10:49:12 -0300

    Lesson 4 arrays and slices final commit

--- 4-arrays_slices/sum.go
+++ 4-arrays_slices/sum.go
@@ -29,3 +29,17 @@ func SumAll2(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
