875cad06ddfc2af6cd548168e6988293fefa656c
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-13 07:12:56 -0300

    Lesson 7 - maps final commit

--- 7-maps/dictionary.go
+++ 7-maps/dictionary.go
@@ -1,10 +1,13 @@
 package dictionary
 
-import "errors"
-
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
 
 type Dictionary map[string]string
+type DictionaryErr string
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,4 +17,42 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
+func (e DictionaryErr) Error() string {
+	return string(e)
 }
