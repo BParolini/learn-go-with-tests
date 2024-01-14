218373efce987cb8befcab9afe574ccf00f907e2
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-14 15:44:37 -0300

    Renamed folders to guarantee correct order

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -15,12 +15,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 type ConfigurableSleeper struct {
 	duration time.Duration
 	sleep    func(time.Duration)
