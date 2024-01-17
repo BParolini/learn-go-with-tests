fd9c5303a4a50c26a518e95a616c73b5acafe7b6
Bruno Parolini <bruno.parolini@olxbr.com>
2024-01-16 22:51:09 -0300

    Lesson 16 - mathematics first commit

---
+++ 16-mathematics/clockface.go
@@ -0,0 +1,35 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two-dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point
+func SecondHand(t time.Time) Point {
+	p := secondHandPoint(t)
+	p = Point{p.X * 90, p.Y * 90}   //scale
+	p = Point{p.X, -p.Y}            //flip
+	p = Point{p.X + 150, p.Y + 150} //translate
+
+	return p
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / float64(t.Second())))
+}
---
+++ 16-mathematics/clockface_acceptance_test.go
@@ -0,0 +1,28 @@
+package clockface
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
